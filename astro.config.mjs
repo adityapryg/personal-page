33b8d3819457596da52c256ce996a638dd68a10b
adityapryg <aditya@brainmatics.com>
2024-07-22 06:20:30 +0700

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -7,8 +9,10 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://adityapryg.my.id",
-  base: "personal-page",
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
