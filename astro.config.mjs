916a74d818d46839fcb94378ba518aed85e3a1ca
adityapryg <aditya@brainmatics.com>
2024-07-22 06:26:22 +0700

    remove edgemiddleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import netlify from '@astrojs/netlify';
-import netlify from '@astrojs/netlify/functions';
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -10,9 +9,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  adapter: netlify(),
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
